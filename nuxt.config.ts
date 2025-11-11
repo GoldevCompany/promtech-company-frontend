1d2fdb4b0c520e8d635341e922b16cac57953e52
Danya_Mochalov <volzhankaonelove@yandex.ru>
2025-11-11 19:00:59 +0400

    feat: Link & Icon conmponents, dev UI page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  compatibilityDate: '2025-11-11',
   devtools: { enabled: true },
+  modules: ['@nuxt/icon'],
+  icon: {
+    customCollections: [
+      {
+        prefix: 'promtech-icons',
+        dir: './app/assets/icons',
+      },
+    ],
+  },
     vite: {
         css: {
             preprocessorOptions: {
